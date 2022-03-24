ff78cbfc293ec25db2f1ff086e9495d27e25c6c4
gayu4 <100912550+gayu4@users.noreply.github.com>
2022-03-24 19:15:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+const navBtn = document.getElementById('nav-btn');
+const cancelBtn = document.getElementById('cancel-btn');
+const sideNav = document.getElementById('sidenav');
+const modal = document.getElementById('modal');
+
+navBtn.addEventListener("click", function(){
+    sideNav.classList.add('show');
+    modal.classList.add('showModal');
+
+});
+
+cancelBtn.addEventListener('click', function(){
+    sideNav.classList.remove('show');
+    modal.classList.add('showmodal');
+});
+
+window.addEventListener('click', function(event){
+    if(event.target === modal){
+        sideNav.classList.remove('show');
+        modal.classList.remove('showModal');
+    }
+});
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
